3fe9c2af625248ff401eefc5b6fa383a5f8d3603
Johan Estrada <johan.manuel.estrada.plaza@gmail.com>
2026-03-21 18:28:16 -0300

    Modificacion test

--- backend/src/test/java/cl/spa/backend/BackendApplicationTests.java
+++ backend/src/test/java/cl/spa/backend/BackendApplicationTests.java
@@ -7,9 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
 public class BackendApplicationTests {
-    
+
     @Test
     void crearPlanCorrectamente() {
 
